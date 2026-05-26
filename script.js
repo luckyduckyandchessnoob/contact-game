9cd6e509e11dbb6c885d2c97a34d74de51c0e7c8
Abby <yin.abigail@gmail.com>
2026-05-25 21:43:04 -0400

    a

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 document.getElementById("contact-btn").addEventListener("click", activateContact);
 let secretWord = document.getElementById("secret-word");
-document.getElementById("guess-input-1").style.display = "none";
-document.getElementById("guess-input-2").style.display = "none";
-document.getElementById("submit-btn").style.display = "none";
-document.getElementById("guess-text-1").style.display = "none";
-document.getElementById("guess-text-2").style.display = "none";
 
 function getWord() {
     return fetch("words.json")
