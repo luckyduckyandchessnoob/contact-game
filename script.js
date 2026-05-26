ba39e706d993df9b6bbce2e1a3cf02385cb73892
Abby <yin.abigail@gmail.com>
2026-05-26 14:18:55 -0400

    z

--- script.js
+++ script.js
@@ -21,8 +21,24 @@ function activateContact() {
     document.getElementById("contact-btn").style.display = "none";
     document.getElementById("guess-text-1").style.display = "block";
     document.getElementById("guess-input-1").style.display = "block";
-    const submitBtn = document.querySelector(".submit-btn");
-    if (submitBtn) {
-        submitBtn.style.display = "block";
-    }
+    document.getElementById("submit-btn-1").style.display = "block";
+    submitBtn.addEventListener("click", () => {
+        const guessInput1 = document.getElementById("guess-input-1").value.trim().toUpperCase();
+        
+        document.getElementById("guess-text-1").style.display = "none";
+        document.getElementById("guess-input-1").style.display = "none";
+        document.getElementById("submit-btn-1").style.display = "none";
+
+        document.getElementById("guess-text-2").style.display = "block";
+        document.getElementById("guess-input-2").style.display = "block";
+        document.getElementById("submit-btn-2").style.display = "block";
+
+        const guessInput2 = document.getElementById("guess-input-2").value.trim().toUpperCase();
+    });
+
+    input1.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            submitBtn.click();
+        }
+    });
 }
