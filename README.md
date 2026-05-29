bccd02c7e08cbb930eaff35ccdc9897ef9921207
Abby <yin.abigail@gmail.com>
2026-05-29 13:09:22 -0400

    i

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Contact! Digital version
 The original game Contact! is a folk game for three or more players. One player is the wordmaster, and the other players try to guess the word by giving each other possible clues. This is the digital version of the game for two or more players. Come back daily for a new word!
-## How to play
+## How to 
+The goal of the game is to reveal the secret word. The secret word is always 6–8 letters.
 1. The first letter of the secret word will be revealed at the top of the screen. The players say any clue for a word that starts with that letter.
 2. If another player thinks they understand the clue, they can press the "Contact!" button. Then both players enter their guess.
 3. Now three things can happen: if both players guessed the secret word correctly, they win. If they guessed incorrectly but they guessed the same word, another letter will be revealed. If they had different guesses, nothing happens. (It doesn't matter if one of them guessed the right word.)
@@ -9,7 +10,7 @@ The original game Contact! is a folk game for three or more players. One player 
 1. The first letter of the secret word is L.
 2. One player gives a clue: "It's in milk."
 3. Another player presses Contact! and they both guess "lactose."
-4. Another letter is revealed: LO.
+4. Another letter is revealed so the computer now says LO.
 5. One player says, "It's a red sea creature."
 6. They and another player guess "lobster."
 7. The players guessed the right word and win!
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let secretWord = document.getElementById("secret-word");
+let guess1 = "";
+let guess2 = "";
+let results = "";
+let wordIndex = 0;
 
 function getWord() {
     return fetch("words.json")
@@ -17,29 +21,58 @@ getWord().then(word => {
     secretWord.innerText = word[0] + "...";
 });
 
-function activateContact() {
-    // show guess input for player 1
-    document.getElementById("contact-btn").style.display = "none";
-    document.getElementById("guess-1").style.display = "flex";
-    document.getElementById("submit-btn-1").addEventListener("click", () => {
-        const guess1 = document.getElementById("guess-input-1").value.trim().toUpperCase();
-        document.getElementById("guess-1").style.display = "none";
-        document.getElementById("guess-2").style.display = "flex";
-    });
-
-    document.getElementById("guess-input-1").addEventListener("keydown", (event) => {
+document.getElementById("guess-input-1").addEventListener("keydown", (event) => {
         if (event.key === "Enter") {
             document.getElementById("submit-btn-1").click();
         }
     });
 
     // show guess input for player 2
-    document.getElementById("submit-btn-2").addEventListener("click", () => {
-        const guess2 = document.getElementById("guess-input-2").value.trim().toUpperCase();
-    });
-    document.getElementById("guess-input-2").addEventListener("keydown", (event) => {
-        if (event.key === "Enter") {
-            document.getElementById("submit-btn-2").click();
+document.getElementById("submit-btn-2").addEventListener("click", () => {
+    guess2 = document.getElementById("guess-input-2").value.trim().toUpperCase();
+    document.getElementById("guess-2").style.display = "none";
+    // compare guesses
+    if (guess1 === guess2) {
+        // win
+        if (guess1 === word) {
+            results += "🎉"
+            secretWord.innerText = "Results: " + results;
         }
-    });
+        // get another letter
+        else {
+            results += "✅";
+            wordIndex++;
+            secretWord.innerText = word.slice(0, wordIndex) + "...";
+            // if they got to the end
+            if (wordIndex >= word.length) {
+                secretWord.innerText = "Results: " + results;
+            }
+        }
+    }
+    // another round
+    else {
+        results += "❌";
+    }
+    if (guess1 !== word) {
+        document.getElementById("contact-btn").style.display = "block";
+        document.getElementById("guess-input-1").value = "";
+        document.getElementById("guess-input-2").value = "";
+    }
+});
+
+document.getElementById("guess-input-2").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        document.getElementById("submit-btn-2").click();
+    }
+});
+
+function activateContact() {
+    // show guess input for player 1
+    document.getElementById("contact-btn").style.display = "none";
+    document.getElementById("guess-1").style.display = "flex";
+    document.getElementById("submit-btn-1").onclick = () => {
+        guess1 = document.getElementById("guess-input-1").value.trim().toUpperCase();
+        document.getElementById("guess-1").style.display = "none";
+        document.getElementById("guess-2").style.display = "flex";
+    };
 }
